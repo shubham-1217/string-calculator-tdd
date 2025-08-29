8107dd5062b53dc2d5b453b4b972c1627baa1cb3
shubham <bablujangra1770@gmail.com>
2025-08-29 18:34:27 +0530

    Add test for single number

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,8 +3,8 @@ import 'package:string_calculator/string_calculator.dart';
 
 void main() {
   group('StringCalculator', () {
-    test('should return 0 for empty string', () {
-      expect(StringCalculator.add(''), equals(0));
+    test('should return the number itself for single number', () {
+    expect(StringCalculator.add('1'), equals(1));
     });
   });
 }
