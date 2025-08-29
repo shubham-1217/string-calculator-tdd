b617189d041e771664d0bbfdd84ca11cbec4cf58
shubham <bablujangra1770@gmail.com>
2025-08-29 18:44:07 +0530

    Add test for multiple negative numbers in exception

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,9 +3,11 @@ import 'package:string_calculator/string_calculator.dart';
 
 void main() {
   group('StringCalculator', () {
-    test('should throw exception for negative numbers', () {
-    expect(() => StringCalculator.add('1,-2,3'), 
-      throwsA(predicate((e) => e is FormatException && e.toString().contains('negative numbers not allowed'))));
+    test('should show all negative numbers in exception message', () {
+    expect(() => StringCalculator.add('1,-2,-3,4'), 
+      throwsA(predicate((e) => e is FormatException && 
+          e.toString().contains('-2') && 
+          e.toString().contains('-3'))));
     });
   });
 }
